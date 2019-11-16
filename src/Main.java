976569a0d6cce75714acaddf8a7591eba28ead3f
Guilherme Avelino <gavelino@gmail.com>
2019-11-16 08:54:03 -0300

    test2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Novo Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println2(mainMenu.getSelection() + "foi selecionada");
+		System.out.println22(mainMenu.getSelection() + "foi selecionada");
 		System.out.println("Fim");
 	}
 
